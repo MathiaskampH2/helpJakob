d136a20cd9b7775101acffd318689a927604eb79
Mathias Wriedt Kamp <math260d@zbc.dk>
2020-11-09 10:35:22 +0100

    Added comment to MessageCarrier.cs

--- helpJakob/MessageCarrier.cs
+++ helpJakob/MessageCarrier.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace HelpJakob
 {
+    /// <summary>
+    /// This enum has the purpose of making two types of MessageCarrier which you can use in MessageSender class.
+    /// </summary>
     public enum MessageCarrier
     {
         Smtp,
